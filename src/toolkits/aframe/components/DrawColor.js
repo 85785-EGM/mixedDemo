e8528257dcb3a5b444221204b0404beeb60916d0
85785-EGM <2307104918@qq.com>
2022-07-26 14:01:53 +0800

    test vertex color

---
+++ src/toolkits/aframe/components/DrawColor.js
@@ -0,0 +1,21 @@
+import { BufferAttribute } from 'super-three'
+
+export default {
+  dependencies: [],
+  schema: {},
+  init () {},
+  update () {
+    const mesh = this.el.getObject3D('mesh')
+    mesh.material.vertexColors = true
+
+    const count = mesh.geometry.getAttribute('position').count
+    const color = new BufferAttribute(new Float32Array(count * 3), 3, true)
+    for (let i = 0; i < count; i++) {
+      color.setXYZ(i, 1, 0, 0)
+    }
+    mesh.geometry.setAttribute('color', color)
+    color.needsUpdate = true
+    mesh.geometry.needsUpdate = true
+    mesh.needsUpdate = true
+  }
+}
---
+++ src/views/test/Color.vue
@@ -0,0 +1,30 @@
+<template>
+  <a-scene
+    auto-background
+    vr-mode-ui="enabled: false"
+    axes-helper="visible: true"
+  >
+    <a-box
+      id="asdf"
+      radius="10"
+      position="0 0 0"
+      height="20"
+      width="20"
+      depth="20"
+      material="color: #8cdcfe;flatShading:true;opacity:1;wireframe:false"
+      visible="true"
+      draw-color
+    />
+    <a-cam
+      id="camera"
+      camera="fov:20;near:1;far:5000"
+      camera-controls
+      clicker
+    />
+    <a-entity auto-light="intensity: 1;" />
+  </a-scene>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
