18ea2707afe464d5da0d372b482694b343f4d81e
85785-EGM <2307104918@qq.com>
2022-07-27 15:54:11 +0800

    finish clipping component

--- src/router/testHelper.js
+++ src/router/testHelper.js
@@ -1,2 +1,3 @@
+export { default as Clipping } from '@/views/test/Clipping.vue'
 export { default as Cutting } from '@/views/test/Cutting.vue'
 export { default as MeshCut } from '@/views/test/MeshCut.vue'
